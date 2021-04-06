000ad5b545357c3113ce509a6472ae736f63b9fd
dmm20210201 <tnaoki.09j@gmail.com>
2021-04-06 10:36:58 +0000

    Start_PF作成

--- db/migrate/20210405123616_create_users.rb
+++ db/migrate/20210405123616_create_users.rb
@@ -1,10 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :name
       t.string :name_kana
-      t.string :email
-      t.string :encrypted_password
       t.string :affiliation
       t.text :introduction
       t.string :profile_image
