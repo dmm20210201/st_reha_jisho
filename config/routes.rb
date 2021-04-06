3157c3bcb94709a23993e966f0cef477f02ebdbd
dmm20210201 <tnaoki.09j@gmail.com>
2021-04-06 10:14:20 +0000

    StartPF作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'homes#top'
+  get 'homes/about'
+  resources :users
+  resources :rehabilitations
 end
---
+++ test/controllers/rehabilitations_controller_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class RehabilitationsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get rehabilitations_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get rehabilitations_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get rehabilitations_create_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get rehabilitations_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get rehabilitations_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get rehabilitations_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get rehabilitations_destroy_url
+    assert_response :success
+  end
+
+end
