6b639b5ed6fb98e019f9b6d718751add4c779556
dmm20210201 <tnaoki.09j@gmail.com>
2021-04-14 15:32:36 +0000

    Add_コメント非同期通信

--- app/controllers/rehabilitation_comments_controller.rb
+++ app/controllers/rehabilitation_comments_controller.rb
@@ -1,15 +1,17 @@
 class RehabilitationCommentsController < ApplicationController
   def create
-    rehabilitation = Rehabilitation.find(params[:rehabilitation_id])
+    @rehabilitation = Rehabilitation.find(params[:rehabilitation_id])
     comment = current_user.rehabilitation_comments.new(rehabilitation_comment_params)
-    comment.rehabilitation_id = rehabilitation.id
+    comment.rehabilitation_id = @rehabilitation.id
     comment.save
-    redirect_to rehabilitation_path(rehabilitation.id)
   end
 
   def destroy
-    RehabilitationComment.find_by(id: params[:id], rehabilitation_id: params[:rehabilitation_id]).destroy
-    redirect_to rehabilitation_path(params[:rehabilitation_id])
+    # RehabilitationComment.find_by(id: params[:id], rehabilitation_id: params[:rehabilitation_id]).destroy
+    @rehabilitation = Rehabilitation.find(params[:rehabilitation_id])
+    comment = current_user.rehabilitation_comments.find_by(rehabilitation_comment_params)
+    comment.destroy
+    # redirect_to rehabilitation_path(params[:rehabilitation_id])
   end
 
   private
