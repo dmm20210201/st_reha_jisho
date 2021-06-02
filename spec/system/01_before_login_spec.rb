95fab4c32e1a3a8259d590a5609165995826e018
dmm20210201 <tnaoki.09j@gmail.com>
2021-06-02 17:11:40 +0000

    [Update]RSpec_01_before_login_spec

--- spec/system/01_before_login_spec.rb
+++ spec/system/01_before_login_spec.rb
@@ -1,3 +1,46 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
+describe '[STEP1] ユーザログイン前のテスト' do
+  describe 'トップ画面のテスト' do
+    before do
+      visit root_path
+    end
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/'
+      end
+      it 'ログインリンクが表示される: 左上から3番目のリンクが「ログイン」である' do
+        log_in_link = find_all('a')[3].native.inner_text
+        expect(log_in_link).to match(/ログイン/i)
+      end
+      it 'ログインリンクの内容が正しい' do
+        log_in_link = find_all('a')[3].native.inner_text
+        expect(page).to have_link log_in_link, href: new_user_session_path
+      end
+      it '新規登録リンクが表示される: 左上から4番目のリンクが「新規登録」である' do
+        sign_up_link = find_all('a')[4].native.inner_text
+        expect(sign_up_link).to match(/新規登録/i)
+      end
+      it '新規登録リンクの内容が正しい' do
+        sign_up_link = find_all('a')[4].native.inner_text
+        expect(page).to have_link sign_up_link, href: new_user_registration_path
+      end
+    end
+  end
+
+  describe 'アバウト画面のテスト' do
+    before do
+      visit '/homes/about'
+    end
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
+        expect(homes_about_path).to eq '/homes/about'
+      end
+    end
+  end
+
+end
