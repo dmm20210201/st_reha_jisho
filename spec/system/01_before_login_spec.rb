954cc77e7fc81c2aebcb7a1367bd9a3f8eba03bc
dmm20210201 <tnaoki.09j@gmail.com>
2021-06-06 17:58:09 +0000

    [Update]RSpecテストコード追加

--- spec/system/01_before_login_spec.rb
+++ spec/system/01_before_login_spec.rb
@@ -43,4 +43,51 @@ describe '[STEP1] ユーザログイン前のテスト' do
     end
   end
 
+  describe 'ヘッダーのテスト: ログインしていない場合' do
+    before do
+      visit root_path
+    end
+
+    context '表示内容の確認' do
+      it 'タイトルが表示される' do
+        expect(page).to have_content 'STリハ辞書'
+      end
+      it 'Topリンクが表示される: 左上から1番目のリンクが「Top」である' do
+        top_link = find_all('a')[1].native.inner_text
+        expect(top_link).to match(/top/i)
+      end
+      it 'Aboutリンクが表示される: 左上から2番目のリンクが「About」である' do
+        about_link = find_all('a')[2].native.inner_text
+        expect(about_link).to match(/about/i)
+      end
+      it 'ログインリンクが表示される: 左上から3番目のリンクが「ログイン」である' do
+        login_link = find_all('a')[3].native.inner_text
+        expect(login_link).to match(/ログイン/i)
+      end
+      it '新規登録リンクが表示される: 左上から4番目のリンクが「新規登録」である' do
+        sign_up_link = find_all('a')[4].native.inner_text
+        expect(sign_up_link).to match(/新規登録/i)
+      end
+    end
+
+    context 'リンクの内容を確認' do
+      subject { current_path }
+
+      it 'Topを押すと、トップ画面に遷移する' do
+        top_link = find_all('a')[1].native.inner_text
+        top_link = top_link.delete(' ')
+        top_link.gsub!(/\n/, '')
+        click_link top_link
+        is_expected.to eq '/'
+      end
+      it 'Aboutを押すと、アバウト画面に遷移する' do
+        about_link = find_all('a')[2].native.inner_text
+        about_link = about_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
+        click_link about_link
+        is_expected.to eq '/homes/about'
+      end
+    end
+
+  end
+
 end
